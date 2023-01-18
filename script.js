459be4fd5670d07c907f09bf213b60f203442743
Rafael Alves <rafaelalvesras2@outlook.com>
2023-01-18 20:47:48 -0300

    atualizando entrega

---
+++ script.js
@@ -0,0 +1,119 @@
+let products = [
+    {
+        imagem: "./assets/img/amaciante conforto.png",
+        nome: "Amaciante Conforto",
+        descricao: "Amaciante conforto é indicado para lavagem de tecidos ou roupas, mantendo as roupas de sua família macias.",
+        preco: 7.99
+    },
+    {
+        imagem: "./assets/img/amaciante carinho.png",
+        nome: "Amaciante Carinho",
+        descricao: "Amaciante conforto é indicado para lavagem de tecidos ou roupas brancas e coloridos mantendo as roupas de sua família macias.",
+        preco: 7.99
+
+    },
+    {
+        imagem: "./assets/img/alvejante.png",
+        nome: "Alvejante",
+        descricao: "Alvejante sem Cloro  é indicado para roupas brancas ou coloridas pois não contém cloro.",
+        preco: 10.99
+    },
+    {
+        imagem: "./assets/img/clareador de pisos.png",
+        nome: "Clareador de Pisos",
+        descricao: "Clareador de pisos é um produto que veio pra trazer praticidade e segurança na desincrustação de pisos e azulejos.",
+        preco: 22.00
+    },
+    {
+        imagem: "./assets/img/lava roupas ação profunda.png",
+        nome: "Lava Roupas Ação Profunda",
+        descricao: "Lava roupas ação profunda é indicado para lavagem simples ou  pesada de roupas.",
+        preco: 13.99
+    },
+    {
+        imagem: "./assets/img/lava roupas de côco.png",
+        nome: "Lava Roupas de Côco",
+        descricao: "Lava roupas côco, oferece o máximo cuidado com as roupas para as peles delicadas.",
+        preco: 13.99
+    },
+    {
+        imagem: "./assets/img/lava louças neutro.png",
+        nome: "Lava Louças Neutro",
+        descricao: "Produto indicado para lavagem de louças, copos, talheres e utensílios domésticos.",
+        preco: 8.99
+    },
+    {
+        imagem: "./assets/img/limpador de aluminios.png",
+        nome: "Limpador de Alumínios",
+        descricao: "Produto indicado para limpeza de utensílios de alumínio em geral.",
+        preco: 13.99
+    },
+    {
+        imagem: "./assets/img/multiuso.png",
+        nome: "Multiuso",
+        descricao: "Produto indicado para limpeza geral em pisos, azulejos, banheiros, cozinhas, móveis, utensílios domésticos, eletrodomésticos etc.",
+        preco: 10.99
+    },
+    {
+        imagem: "./assets/img/pinho gel.png",
+        nome: "Pinho Gel",
+        descricao: "Limpa Pisos Pinho Gel, Possuí força máxima na limpeza de pisos e azulejos e qualquer superfície lavável em qualquer parte da casa.",
+        preco: 12.99
+    }
+]
+
+let vitrine = document.querySelector("ul")
+const ulCar = document.querySelector(".divCarrinho")
+const divCar = document.querySelector(".carrinhoList")
+
+function renderProducts() {
+    for (let i = 0; i < products.length; i++) {
+        let listCard = document.createElement("li")
+        vitrine.appendChild(listCard)
+        let img = document.createElement("img")
+        listCard.appendChild(img)
+        img.src = products[i].imagem
+        img.alt = "produto de limpeza"
+        let nome = document.createElement("h2")
+        listCard.appendChild(nome)
+        nome.innerText = products[i].nome
+        let descricao = document.createElement("p")
+        listCard.appendChild(descricao)
+        descricao.innerText = products[i].descricao
+        let preco = document.createElement("h3")
+        listCard.appendChild(preco)
+        preco.innerText = products[i].preco.toFixed(2)
+        let buttonAdd = document.createElement("button")
+        listCard.appendChild(buttonAdd)
+        buttonAdd.innerText = "Adicionar ao Carrinho"
+        buttonAdd.classList.add("buttonAdd")
+        buttonAdd.addEventListener("click", function () {
+            const listaDeProduto = document.createElement("li")
+            divCar.appendChild(listaDeProduto)
+            const productName = document.createElement("p")
+            listaDeProduto.appendChild(productName)
+            productName.innerText = products[i].nome            
+            const valorDoProduto = document.createElement("p")
+            valorDoProduto.innerText = products[i].preco
+            listaDeProduto.appendChild(valorDoProduto)
+        })
+    }
+
+}
+
+renderProducts()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
